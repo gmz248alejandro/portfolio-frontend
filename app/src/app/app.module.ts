8bc7a42e34b342bdca9e7cab9eb21cf316f05906
Alejandro Javier Gómez <gmz248alejandro@gmail.com>
2022-10-12 03:41:35 -0300

    Migración a Angular realizada

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContainerComponent } from './components/container/container.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ContainerComponent,
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
